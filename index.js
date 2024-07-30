073a71769bb4bfbe322d31ed595c7091216ae88b
Avnish Patel <avnishpatel855@gmail.com>
2024-07-30 19:28:54 +0530

    pain day

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("hellooooo");
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const slider = document.querySelector('.slick-slider');
+    const slides = document.querySelectorAll('.element');
+    const totalSlides = slides.length;
+    let currentIndex = 0;
+
+
+
+
+    function showSlide(index) {
+      // Ensure the index is within the bounds
+      if (index >= totalSlides) {
+        currentIndex = 0;
+      } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+      } else {
+        currentIndex = index;
+      }
+      slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    // Event listeners for the buttons
+    document.getElementById('left-button').addEventListener('click', () => {
+     (showSlide(currentIndex - 1)) 
+    });
+
+    document.getElementById('right-button').addEventListener('click', () => {
+      showSlide(currentIndex + 1);
+
+    
+    });
+
+    // Initial slide
+    showSlide(currentIndex);
+  });
+
